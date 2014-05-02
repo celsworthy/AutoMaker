b7ed88b341e463afdc01c1d8d65e8499ffe531c1
Ian Hudson <ian@cel-uk.com>
2014-05-02 01:34:49 +0100

    Changed to use new shutdown process.

--- src/main/java/celtech/automaker/AutoMaker.java
+++ src/main/java/celtech/automaker/AutoMaker.java
@@ -6,24 +6,32 @@
 package celtech.automaker;
 
 import celtech.appManager.ApplicationMode;
+import celtech.appManager.TaskController;
 import celtech.configuration.ApplicationConfiguration;
 import celtech.coreUI.DisplayManager;
+import celtech.printerControl.Printer;
+import celtech.printerControl.PrinterStatusEnumeration;
 import celtech.printerControl.comms.RoboxCommsManager;
 import celtech.utils.AutoUpdate;
 import celtech.utils.AutoUpdateCompletionListener;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import libertysystems.configuration.ConfigNotLoadedException;
 import libertysystems.configuration.Configuration;
 import libertysystems.stenographer.Stenographer;
 import libertysystems.stenographer.StenographerFactory;
+import org.controlsfx.control.action.Action;
+import org.controlsfx.dialog.Dialogs;
 
 /**
  *
@@ -35,15 +43,19 @@ public class AutoMaker extends Application implements AutoUpdateCompletionListen
     private static final Stenographer steno = StenographerFactory.getStenographer(AutoMaker.class.getName());
     private static Configuration configuration = null;
     private static DisplayManager displayManager = null;
+    private ResourceBundle i18nBundle = null;
     private RoboxCommsManager commsManager = null;
     private AutoUpdate autoUpdater = null;
+    private Dialogs.CommandLink dontShutDown = null;
+    private Dialogs.CommandLink shutDownAndTerminate = null;
+    private Dialogs.CommandLink shutDownWithoutTerminating = null;
 
     @Override
     public void start(Stage stage) throws Exception
     {
         stage.getIcons().addAll(new Image(getClass().getResourceAsStream("/celtech/automaker/resources/images/AutomakerIcon_256x256.png")),
-                new Image(getClass().getResourceAsStream("/celtech/automaker/resources/images/AutomakerIcon_64x64.png")),
-                new Image(getClass().getResourceAsStream("/celtech/automaker/resources/images/AutomakerIcon_32x32.png")));
+                                new Image(getClass().getResourceAsStream("/celtech/automaker/resources/images/AutomakerIcon_64x64.png")),
+                                new Image(getClass().getResourceAsStream("/celtech/automaker/resources/images/AutomakerIcon_32x32.png")));
 
         commsManager = RoboxCommsManager.getInstance(ApplicationConfiguration.getApplicationInstallDirectory(AutoMaker.class));
 
@@ -56,16 +68,21 @@ public class AutoMaker extends Application implements AutoUpdateCompletionListen
         }
 
         displayManager = DisplayManager.getInstance();
+        i18nBundle = DisplayManager.getLanguageBundle();
 
-        String applicationName = DisplayManager.getLanguageBundle().getString("application.title");
+        String applicationName = i18nBundle.getString("application.title");
         displayManager.configureDisplayManager(stage, applicationName);
+
+        dontShutDown = new Dialogs.CommandLink(i18nBundle.getString("dialogs.dontShutDownTitle"), i18nBundle.getString("dialogs.dontShutDownMessage"));
+        shutDownAndTerminate = new Dialogs.CommandLink(i18nBundle.getString("dialogs.shutDownAndTerminateTitle"), i18nBundle.getString("dialogs.shutDownAndTerminateMessage"));
+        shutDownWithoutTerminating = new Dialogs.CommandLink(i18nBundle.getString("dialogs.shutDownAndDontTerminateTitle"), i18nBundle.getString("dialogs.shutDownAndDontTerminateMessage"));
 
         VBox statusSupplementaryPage = null;
 
         try
         {
             URL mainPageURL = getClass().getResource("/celtech/automaker/resources/fxml/SupplementaryStatusPage.fxml");
-            FXMLLoader configurationSupplementaryStatusPageLoader = new FXMLLoader(mainPageURL, DisplayManager.getLanguageBundle());
+            FXMLLoader configurationSupplementaryStatusPageLoader = new FXMLLoader(mainPageURL, i18nBundle);
             statusSupplementaryPage = (VBox) configurationSupplementaryStatusPageLoader.load();
         } catch (IOException ex)
         {
@@ -80,20 +97,55 @@ public class AutoMaker extends Application implements AutoUpdateCompletionListen
             statusSlideOutHandle.getChildren().add(statusSupplementaryPage);
             VBox.setVgrow(statusSupplementaryPage, Priority.ALWAYS);
         }
-        
+
+        stage.setOnCloseRequest((WindowEvent event) ->
+        {
+            boolean transferringDataToPrinter = false;
+
+            for (Printer printer : commsManager.getPrintStatusList())
+            {
+                transferringDataToPrinter = printer.getPrintQueue().sendingDataToPrinterProperty().get();
+                if (transferringDataToPrinter)
+                {
+                    Action shutDownResponse = Dialogs.create().title(i18nBundle.getString("dialogs.printJobsAreStillTransferringTitle"))
+                            .message(i18nBundle.getString("dialogs.printJobsAreStillTransferringMessage"))
+                            .masthead(null)
+                            .showCommandLinks(dontShutDown, dontShutDown, shutDownAndTerminate);
+
+                    if (shutDownResponse == dontShutDown)
+                    {
+                        event.consume();
+                    }
+                    break;
+                }
+            }
+        });
+
+        final AutoUpdateCompletionListener completeListener = this;
+
+        stage.setOnShown((WindowEvent event) ->
+        {
+            autoUpdater = new AutoUpdate("AutoMaker", completeListener);
+            autoUpdater.start();
+        });
 
         stage.show();
-        
-        autoUpdater = new AutoUpdate("AutoMaker", this);
-        autoUpdater.start();
     }
 
     @Override
     public void stop() throws Exception
     {
         autoUpdater.shutdown();
-        commsManager.shutdown();
         displayManager.shutdown();
+        commsManager.shutdown();
+
+        TaskController taskController = TaskController.getInstance();
+
+        if (taskController.getNumberOfManagedTasks() > 0)
+        {
+            Thread.sleep(5000);
+            taskController.shutdownAllManagedTasks();
+        }
     }
 
     /**
@@ -115,8 +167,7 @@ public class AutoMaker extends Application implements AutoUpdateCompletionListen
         if (requiresShutdown)
         {
             Platform.exit();
-        }
-        else
+        } else
         {
             commsManager.start();
         }

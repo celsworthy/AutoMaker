350ff7f9b9767d17eba101b4aa2c83e1a4fd58a5
Tony Aldhous <tonyaldhous@cel-uk.com>
2018-02-02 14:21:59 +0000

    Add debug statements.

--- src/main/java/celtech/automaker/AutoMaker.java
+++ src/main/java/celtech/automaker/AutoMaker.java
@@ -50,7 +50,9 @@ public class AutoMaker extends Application implements AutoUpdateCompletionListen
 
     static
     {
+        System.out.println("AutoMaker:Getting stenographer.");
         steno = StenographerFactory.getStenographer(AutoMaker.class.getName());
+        System.out.println("AutoMaker:Got stenographer.");
     }
     private static DisplayManager displayManager = null;
     private ResourceBundle i18nBundle = null;
